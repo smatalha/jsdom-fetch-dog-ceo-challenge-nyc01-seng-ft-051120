222b6d7f4a1174fd005055aedaac8ec8f6c989e5
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-06-26 01:16:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ console.log('%c HI', 'color: firebrick')
 
 document.addEventListener('DOMContentLoaded', function () {
 
-  function addImages() {
-    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-    fetch(imgUrl)
-    // .then(res=> res.json())
-    // .then(results => {
-      // results.messages.forEach(image => addimage(image))
-      console.log(imgUrl);
-    // })
-  }
+});
 
-})
+function addImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+  // .then(res=> res.json())
+  // .then(results => {
+    // results.messages.forEach(image => addimage(image))
+    console.log(imgUrl);
+  // })
+}
